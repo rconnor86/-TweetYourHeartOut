a48c88653ef6c8a40dc263d49870af7dca135b8b
Connor <connor.rosentrater@wsu.edu>
2019-02-24 02:59:20 -0800

    Testing

--- Authentication.py
+++ Authentication.py
@@ -1,63 +1,24 @@
 import tweepy
-import json
 import webbrowser
 import emoji
+import tkinter as tk
 
-
-# Specify the account credentials in the following variables:
-consumer_key = 'gvracgdHjxGXcGHPlkI9e2ER5'
-consumer_secret = 'tslCrlX8fWeaaeyu2l9PkgxrpfAE745qk2R47fEYpNCANwOZ2V'
-access_token = '1945189632-v5kQ4eaDu62MYzL2OyF5GV768bwVDkbXrqDXHt4'
-access_token_secret = 'D8CYRTDiN9Ot5NISeLIahqAqdzKGeDcXdV8WfS2yLkB9r'
-file = open("results.csv", "wb")
-
-
-
-# This listener will print out all Tweets it receives
-class PrintListener(tweepy.StreamListener):
-    def on_data(self, data):
-        # Decode the JSON data
-        tweet = json.loads(data)
-
-        # Print out the Tweet
-        print('@%s: %s' % (tweet['user']['screen_name'], tweet['text'].encode('ascii', 'ignore')))
-
-    def on_error(self, status):
-        print(status)
-
-
-if __name__ == '__main__':
-    listener = PrintListener()
-
-    # Show system message
-    print('I will now analyze your last 50 tweets"!')
-
-    # Authenticate
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-try:
-    token = redirect_url = auth.get_authorization_url()
-    webbrowser.open(redirect_url, new=2)
-except tweepy.TweepError:
-    print ('Error! Failed to get request token.')
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
-#API = tweepy.api
-
-pin = input('Verifier:')
-auth.get_access_token(pin)
-API = tweepy.API(auth)
-API.verify_credentials()
-
-index = 1
-
-for index in range(1, 10):
-    # Connect the stream to our listener
-    tweets = API.user_timeline(count=1220, page=index)
-    for i in tweets:
-        temp = emoji.demojize(i.text) + "\n"
-        temp = temp.replace(',', '')
-        file.write(temp.encode("utf-8"))
-    
-    
+counter = 0 
+def counter_label(label):
+  counter = 0
+  def count():
+    global counter
+    counter += 1
+    label.config(text=str(counter))
+    label.after(1000, count)
+  count()
+ 
+ 
+root = tk.Tk()
+root.title("Counting Seconds")
+label = tk.Label(root, fg="dark green")
+label.pack()
+counter_label(label)
+button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+button.pack()
+root.mainloop()
--- Status.py
+++ Status.py
@@ -1,22 +1,60 @@
-import tweepy
-import requests_oauthlib
-import requests
-import json
+from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E
 
-from requests_oauthlib import OAuth1Session
+class Calculator:
 
-# Specify the account credentials in the following variables:
-consumer_key = 'gvracgdHjxGXcGHPlkI9e2ER5'
-consumer_secret = 'tslCrlX8fWeaaeyu2l9PkgxrpfAE745qk2R47fEYpNCANwOZ2V'
-access_token = '1945189632-v5kQ4eaDu62MYzL2OyF5GV768bwVDkbXrqDXHt4'
-access_token_secret = 'D8CYRTDiN9Ot5NISeLIahqAqdzKGeDcXdV8WfS2yLkB9r'
+    def __init__(self, master):
+        self.master = master
+        master.title("Calculator")
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.total = 0
+        self.entered_number = 0
 
-try:
-    redirect_url = auth.get_authorization_url()
-except tweepy.TweepError:
-    print ("Error! Failed to get request token.")
+        self.total_label_text = IntVar()
+        self.total_label_text.set(self.total)
+        self.total_label = Label(master, textvariable=self.total_label_text)
 
-print(redirect_url)
+        self.label = Label(master, text="Total:")
 
+        vcmd = master.register(self.validate) # we have to wrap the command
+        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+
+        self.add_button = Button(master, text="+", command=lambda: self.update("add"))
+        self.subtract_button = Button(master, text="-", command=lambda: self.update("subtract"))
+        self.reset_button = Button(master, text="Reset", command=lambda: self.update("reset"))
+
+        # LAYOUT
+
+        self.label.grid(row=0, column=0, sticky=W)
+        self.total_label.grid(row=0, column=1, columnspan=2, sticky=E)
+
+        self.entry.grid(row=1, column=0, columnspan=3, sticky=W+E)
+
+        self.add_button.grid(row=2, column=0)
+        self.subtract_button.grid(row=2, column=1)
+        self.reset_button.grid(row=2, column=2, sticky=W+E)
+
+    def validate(self, new_text):
+        if not new_text: # the field is being cleared
+            self.entered_number = 0
+            return True
+
+        try:
+            self.entered_number = int(new_text)
+            return True
+        except ValueError:
+            return False
+
+    def update(self, method):
+        if method == "add":
+            self.total += self.entered_number
+        elif method == "subtract":
+            self.total -= self.entered_number
+        else: # reset
+            self.total = 0
+
+        self.total_label_text.set(self.total)
+        self.entry.delete(0, END)
+
+root = Tk()
+my_gui = Calculator(root)
+root.mainloop()
