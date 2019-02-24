b1dc450c92f755034030feb0178479b63738adaa
Bri0927 <43020510+Bri0927@users.noreply.github.com>
2019-02-23 19:41:44 -0800

    Most Updated
    Doesn't fully work, but pushing so Connor can view and assist with coding on his device.

--- Test.py
+++ Test.py
@@ -1,5 +1,6 @@
 import tweepy
 import json
+import webbrowser
 
 
 # Specify the account credentials in the following variables:
@@ -8,6 +9,7 @@ consumer_secret = 'tslCrlX8fWeaaeyu2l9PkgxrpfAE745qk2R47fEYpNCANwOZ2V'
 access_token = '1945189632-v5kQ4eaDu62MYzL2OyF5GV768bwVDkbXrqDXHt4'
 access_token_secret = 'D8CYRTDiN9Ot5NISeLIahqAqdzKGeDcXdV8WfS2yLkB9r'
 file = "results.txt"
+
 
 
 # This listener will print out all Tweets it receives
@@ -27,13 +29,31 @@ if __name__ == '__main__':
     listener = PrintListener()
 
     # Show system message
-    print('I will now analyze your last 50 tweets"! ==>')
+    print('I will now analyze your last 50 tweets"!')
 
     # Authenticate
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    API.verify_credentials()
+try:
+    token = redirect_url = auth.get_authorization_url()
+    webbrowser.open(redirect_url, new=2)
+except tweepy.TweepError:
+    print ('Error! Failed to get request token.')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.request_token = token
+
+API = tweepy.api
+
+
+verifier = input('Verifier:')
+auth.get_access_token(verifier)
+API = tweepy.API(auth)
+API.verify_credentials()
+
+
+
 
     # Connect the stream to our listener
-    file = API.user_timeline([50, count])
+file = API.user_timeline()
    
