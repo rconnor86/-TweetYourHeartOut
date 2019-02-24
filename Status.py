d260e70516eb920bf6430f6859f2c10a764885e2
Connor <connor.rosentrater@wsu.edu>
2019-02-23 17:13:16 -0800

    Creating Status.py

---
+++ Status.py
@@ -0,0 +1,9 @@
+import tweepy
+import json
+
+# Specify the account credentials in the following variables:
+consumer_key = 'gvracgdHjxGXcGHPlkI9e2ER5'
+consumer_secret = 'tslCrlX8fWeaaeyu2l9PkgxrpfAE745qk2R47fEYpNCANwOZ2V'
+access_token = '1945189632-v5kQ4eaDu62MYzL2OyF5GV768bwVDkbXrqDXHt4'
+access_token_secret = 'D8CYRTDiN9Ot5NISeLIahqAqdzKGeDcXdV8WfS2yLkB9r'
+
